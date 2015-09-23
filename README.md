21518e551de7fd9b582a2380d1f8f9d73f7ee046
John Shanley <john.michael.shanley@gmail.com>
2015-09-22 20:46:47 -0400

    initial

--- README.md
+++ README.md
@@ -1,1 +1,64 @@
 # heroku-deploy-tarball
+
+A tiny script for automating the deployment of a tarball to heroku.
+
+## Installation
+
+``` shell
+npm install heroku-deploy-tarball
+```
+
+## Usage
+
+### Example: Single Target
+
+*deploy.js*
+``` javascript
+var deploy = require('heroku-deploy-tarball');
+
+var config = {
+  app: 'my-heroku-app',
+  tarball: 'path/to/build.tar.gz'
+}
+
+deploy(config);
+```
+
+*shell*
+
+``` shell
+node deploy
+```
+
+
+### Example: Multiple Targets
+
+*deploy.js*
+```javascript
+var deploy = require('heroku-deploy-tarball');
+
+var requestedTarget = process.argv[2];
+
+if (!requestedTarget) {
+  console.log('You must specify a deploy target');
+  return;
+}
+
+var targets = {
+  staging: {
+    app: 'my-heroku-staging-app',
+    tarball: 'build.tar.gz'
+  },
+  production: {
+    app: 'my-heroku-production-app',
+    tarball: 'build.tar.gz'
+  }
+};
+
+deploy(targets[requestedTarget]);
+```
+
+*shell*
+``` shell
+node deploy staging
+```
---
+++ index.js
@@ -0,0 +1,87 @@
+var fs = require('fs');
+var path = require('path');
+var request = require('request');
+var netrc = require('node-netrc');
+var chalk = require('chalk');
+var Chance = require('chance');
+var chance = new Chance();
+
+module.exports = function(config) {
+
+  if (!config.app) {
+    console.log(chalk.red('You must specify an app name.'));
+    return;
+  }
+
+  if (!config.tarball) {
+    console.log(chalk.red('You must specify a tarball.'));
+    return;
+  }
+
+  var APP_URL = 'https://api.heroku.com/apps/' + config.app;
+
+  var nrc = netrc('api.heroku.com');
+  var credentials = {
+    username: nrc.login,
+    password: nrc.password
+  };
+
+  var sourceConfig = {
+    method: 'POST',
+    url: APP_URL + '/sources',
+    headers: {
+      'Accept': 'application/vnd.heroku+json; version=3'
+    },
+    auth: credentials
+  };
+
+  console.log(chalk.blue('Getting source urls...'));
+  request(sourceConfig, function(e, res, body) {
+    if (!e) {
+      console.log(chalk.green('SUCCESS!'));
+      var source = JSON.parse(body);
+      var tarball = fs.readFileSync(path.resolve(config.tarball));
+      console.log(chalk.blue('Uploading tarball...'));
+      request({
+        method: 'PUT',
+        url: source.source_blob.put_url,
+        body: tarball
+      }, function(e, res, body) {
+        if (!e) {
+          console.log(chalk.green('SUCCESS!'));
+          console.log(chalk.blue('Creating build...'));
+          request({
+            method: 'POST',
+            url: APP_URL + '/builds',
+            headers: {
+              'Accept': 'application/vnd.heroku+json; version=3'
+            },
+            auth: credentials,
+            json: true,
+            body: {
+              source_blob: {
+                url: source.source_blob.get_url,
+                version: chance.hash()
+              }
+            }
+          }, function(e, res, body) {
+            if (!e) {
+              console.log(chalk.green('SUCCESS!'));
+            } else {
+              handleError(e, res);
+            }
+          });
+        } else {
+          handleError(e, res);
+        }
+      });
+    } else {
+      handleError(e, res);
+    }
+  });
+
+  function handleError(e, res) {
+    console.log(chalk.red('Uh oh, something went wrong...'), e, res.statusCode);
+  }
+
+};
