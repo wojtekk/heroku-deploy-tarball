c02ac933d511d50f7a13cb4b323413fd89790cc5
Wojciech Krawczyk <wojciech.krawczyk@schibsted.com>
2017-07-24 16:34:08 +0200

    Cleanup Conversion to ES6 +eslint

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
-var fs = require('fs');
-var path = require('path');
-var request = require('request');
-var netrc = require('node-netrc');
-var chalk = require('chalk');
-var Chance = require('chance');
-var chance = new Chance();
+const fs = require('fs');
+const path = require('path');
+const request = require('request');
+const netrc = require('node-netrc');
+const chalk = require('chalk');
+const Chance = require('chance');
 
-module.exports = function(config) {
+const chance = new Chance();
+
+module.exports = (config) => {
+  function handleError(e, res) {
+    console.log(chalk.red('Uh oh, something went wrong...'), e, res.statusCode);
+  }
 
   if (!config.app) {
     console.log(chalk.red('You must specify an app name.'));
@@ -18,76 +22,70 @@ module.exports = function(config) {
     return;
   }
 
-  var APP_URL = 'https://api.heroku.com/apps/' + config.app;
+  const APP_URL = `https://api.heroku.com/apps/${config.app}`;
 
-  var credentials = config.credentials;
-  if(!credentials || !credentials.username || !credentials.password) {
-    var nrc = netrc('api.heroku.com');
+  let credentials = config.credentials;
+  if (!credentials || !credentials.username || !credentials.password) {
+    const nrc = netrc('api.heroku.com');
     credentials = {
       username: nrc.login,
-      password: nrc.password
+      password: nrc.password,
     };
   }
 
-  var sourceConfig = {
+  const sourceConfig = {
     method: 'POST',
-    url: APP_URL + '/sources',
+    url: `${APP_URL}/sources`,
     headers: {
-      'Accept': 'application/vnd.heroku+json; version=3'
+      Accept: 'application/vnd.heroku+json; version=3',
     },
-    auth: credentials
+    auth: credentials,
   };
 
   console.log(chalk.blue('Getting source urls...'));
-  request(sourceConfig, function(e, res, body) {
+  request(sourceConfig, (e, res, body) => {
     if (!e) {
-      if(res.statusCode >= 400) {
-        return handleError(source, res);
+      if (res.statusCode >= 400) {
+        return handleError(body, res);
       }
       console.log(chalk.green('SUCCESS!'));
-      var source = JSON.parse(body);
-      var tarball = fs.readFileSync(path.resolve(config.tarball));
+      const source = JSON.parse(body);
+      const tarball = fs.readFileSync(path.resolve(config.tarball));
       console.log(chalk.blue('Uploading tarball...'));
-      request({
+      return request({
         method: 'PUT',
         url: source.source_blob.put_url,
-        body: tarball
-      }, function(e, res, body) {
-        if (!e) {
+        body: tarball,
+      }, (err, res2) => {
+        if (!err) {
           console.log(chalk.green('SUCCESS!'));
           console.log(chalk.blue('Creating build...'));
           request({
             method: 'POST',
-            url: APP_URL + '/builds',
+            url: `${APP_URL}/builds`,
             headers: {
-              'Accept': 'application/vnd.heroku+json; version=3'
+              Accept: 'application/vnd.heroku+json; version=3',
             },
             auth: credentials,
             json: true,
             body: {
               source_blob: {
                 url: source.source_blob.get_url,
-                version: chance.hash()
-              }
-            }
-          }, function(e, res, body) {
-            if (!e) {
+                version: chance.hash(),
+              },
+            },
+          }, (err3, res3) => {
+            if (!err3) {
               console.log(chalk.green('SUCCESS!'));
             } else {
-              handleError(e, res);
+              handleError(err3, res3);
             }
           });
         } else {
-          handleError(e, res);
+          handleError(err, res2);
         }
       });
-    } else {
-      handleError(e, res);
     }
+    return handleError(e, res);
   });
-
-  function handleError(e, res) {
-    console.log(chalk.red('Uh oh, something went wrong...'), e, res.statusCode);
-  }
-
 };
